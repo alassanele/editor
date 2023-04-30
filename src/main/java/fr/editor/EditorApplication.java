5ba0d4802c2d2a59af84faa5949739dca56260c2
Alass <alassanele@gmail.com>
2023-04-30 14:05:49 +0200

    Add endpoint

--- src/main/java/fr/editor/EditorApplication.java
+++ src/main/java/fr/editor/EditorApplication.java
@@ -2,12 +2,24 @@ package fr.editor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class EditorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EditorApplication.class, args);
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<String> getAllBooks() {
+
+        return ResponseEntity.ok().body("Hello editor");
     }
 
 }
